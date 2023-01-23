fe1e491f2e45d5f28ac9b563eea3ac0717014959
Meetyp <meetyogeshkumar.patel@flemingcollege.ca>
2023-01-23 13:07:34 -0500

    Modified both files in the repo clone
     - Deleted a print() statement from script1.py
     - Added one print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of python
 print("This is print statement")
-print("This is print statement 2")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # This is good programming advice
 print("This is the script2 print 1")
 print("This is the script2 print 2 stat")
+print("This is the script2 print 3")
